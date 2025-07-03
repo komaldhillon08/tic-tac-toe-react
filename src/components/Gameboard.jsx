044ff23237905fce52904392099da1b083298cb7
sanjay <sanjay8195850041@gmail.com>
2025-07-03 12:07:07 +0530

    add gameBoard function and work the board

--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -1,16 +1,37 @@
+// import the hooks 
+import { useState } from "react";
+
 const initiaGameBoard = [
-    [null ,null, null],
-    [null ,null, null],
-    [null ,null, null]
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
 ]
+
 export default function GameBoard() {
-    return (
-        <ol id="game-board">
-            {initiaGameBoard.map((row , rowIndex) => <li key={rowIndex}>
-                <ol>
-                    {row.map((playerSymbol, colIndex) => <li key={colIndex}> <button>{playerSymbol}</button></li> )}
-                </ol>
-            </li> )}
-        </ol>
-    );
+    const [GameBoard, setGameBoard] = useState(initiaGameBoard);
+
+    function handleSelectSquae(rowIndex, colIndex) {
+        console.log(">>>>>>>>row",rowIndex);
+        console.log(">>>>>>>col",colIndex);
+        
+        setGameBoard((prevGameBoard) => {
+            const updateBoard = [...prevGameBoard.map((innerArray) => [...innerArray])]
+            updateBoard[rowIndex][colIndex] = 'X';
+            return updateBoard;
+        })
+    }
+
+return (
+    <ol id="game-board">
+        {GameBoard.map((row, rowIndex) => <li key={rowIndex}>
+            <ol>
+                {row.map((playerSymbol, colIndex) => (
+                    <li key={colIndex}>
+                        <button onClick={() => handleSelectSquae(rowIndex ,colIndex )}>{playerSymbol}</button>
+                    </li>
+                ))}
+            </ol>
+        </li>)}
+    </ol>
+);
 }
